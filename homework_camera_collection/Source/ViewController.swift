f85e75b8974aec5e6f66e1bc06fcf3d8c8b80bc6
Ruslan Gazizov <ruslangazizov36@gmail.com>
2022-05-01 22:59:58 +0300

    Add navigation controller in storyboard and basic mapView setup to ViewController

--- homework_camera_collection/Source/ViewController.swift
+++ homework_camera_collection/Source/ViewController.swift
@@ -6,9 +6,65 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
+    // MARK: UI
+    
+    private lazy var mapView: MKMapView = {
+        let mapView = MKMapView()
+        mapView.delegate = self
+        return mapView
+    }()
+    
+    // MARK: Life cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Take or choose photo"
+        configureNavigationBar()
+        configureViews()
     }
+    
+    // MARK: UI configuration
+    
+    private func configureNavigationBar() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            title: "Camera",
+            style: .plain,
+            target: self,
+            action: #selector(didTapCameraButton)
+        )
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "Gallery",
+            style: .plain,
+            target: self,
+            action: #selector(didTapGalleryButton)
+        )
+    }
+    
+    private func configureViews() {
+        view.addSubview(mapView)
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            mapView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    // MARK: Actions
+    
+    @objc private func didTapCameraButton() {
+    }
+    
+    @objc private func didTapGalleryButton() {
+    }
+}
+
+// MARK: - MKMapViewDelegate
+
+extension ViewController: MKMapViewDelegate {
 }
