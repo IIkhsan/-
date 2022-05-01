5e95655cd577b2b7d567e223426a0ee25e93a7e5
Ruslan Gazizov <ruslangazizov36@gmail.com>
2022-05-01 22:09:37 +0300

    Remove redundant comments in AppDelegate and SceneDelegate and move files to 2 separate folders

---
+++ homework_camera_collection/Source/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  homework_camera_collection
+//
+//  Created by ilyas.ikhsanov on 29.04.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+}
---
+++ homework_camera_collection/Source/SceneDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  SceneDelegate.swift
+//  homework_camera_collection
+//
+//  Created by ilyas.ikhsanov on 29.04.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+}
